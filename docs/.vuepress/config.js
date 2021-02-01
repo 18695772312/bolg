bbbb517d8d3dfbb7b6d0ca6de64ec84d12e26750
严春丽 <1002354499@qq.com>
2021-02-01 14:13:44 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+const nav = require("./config/nav");
+module.exports = {
+  title: 'Yan',
+  description: '个人笔记分享',
+  themeConfig: {
+    logo: "/images/logo.jpg",
+    lastUpdated: "2020年11月21日",
+    nav,
+  },
+}
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,29 @@
+const nav = [
+  {
+    text: "首页",
+    link: "/",
+  },
+  {
+    text: "vue",
+    link: "/vue/",
+  },
+  {
+    text: "javascript",
+    link: "/js/",
+    items: [
+      {
+        text: "es6",
+        link: "/js/es6.md",
+      },
+      {
+        text: "js中的数组方法",
+        link: "/js/arr-function.md",
+      },
+    ],
+  },
+  {
+    text: "css",
+    link: "/css/",
+  },
+];
+module.exports = nav;
