c04c1b45d6aab1195aeedbbc3af0429b9c5153d8
严春丽 <1002354499@qq.com>
2021-02-03 15:58:05 +0800

    first commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,51 @@
 const nav = require("./config/nav");
 module.exports = {
-  title: 'Yan',
-  description: '个人笔记分享',
+  title: '上天的🐷',
+  description: '向日葵告诉我，只要面对着阳光努力向上，日子就会变得单纯而美好。',
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  base: '/blog/',
+  
+  theme: 'reco',
   themeConfig: {
-    logo: "/images/logo.jpg",
-    lastUpdated: "2020年11月21日",
+    // 博客配置
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag'      // 默认文案 “标签”
+      }
+    },
+    valineConfig: {
+      appId: 'IBmCaoIoKFEr4VPoSPy1dIQR-gzGzoHsz',// your appId
+      appKey: 'tNhtaflmwIXUfk9x7xlMeIDJ', // your appKey
+    },
+    type: 'blog',
+    // logo: "/assets/img/logo.jpg",
     nav,
+    friendLink: [
+      {
+        title: 'vuepress-theme-reco',
+        desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+        logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: 'https://vuepress-theme-reco.recoluan.com'
+      },
+      {
+        title: '午后南杂',
+        desc: 'Enjoy when you can, and endure when you must.',
+        email: 'recoluan@qq.com',
+        link: 'https://www.recoluan.com'
+      },
+    ],//  友情链接
+    author: 'y_chunli',
+    authorAvatar: '/assets/img/head.jpg',
+    // record: 'ICP 备案文案',
+    // recordLink: 'ICP 备案指向链接',
+    // cyberSecurityRecord: '公安部备案文案',
+    // cyberSecurityLink: '公安部备案指向链接',
   },
 }
--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,29 +1,17 @@
-const nav = [
-  {
+const nav = [{
     text: "首页",
     link: "/",
-  },
-  {
-    text: "vue",
-    link: "/vue/",
-  },
-  {
-    text: "javascript",
-    link: "/js/",
+    icon: 'reco-home'
+  },{
+    text: "其他",
+    icon: "reco-message",
     items: [
       {
-        text: "es6",
-        link: "/js/es6.md",
-      },
-      {
-        text: "js中的数组方法",
-        link: "/js/arr-function.md",
-      },
-    ],
-  },
-  {
-    text: "css",
-    link: "/css/",
-  },
+        text: "GitHub",
+        link: "https://github.com/recoluan",
+        icon: "reco-github"
+      }
+    ]
+  }
 ];
 module.exports = nav;
