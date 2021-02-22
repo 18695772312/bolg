abcf658075268a88e0226e565e9edcc869bd924d
18695772312 <861454974@qq.com>
2021-02-22 16:44:08 +0800

    版本更新

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,16 +2,21 @@ const nav = [{
     text: "首页",
     link: "/",
     icon: 'reco-home'
-  },{
+  },
+  {
+    text: 'TimeLine',
+    link: '/timeline/',
+    icon: 'reco-date'
+  },
+  {
     text: "其他",
     icon: "reco-message",
-    items: [
-      {
-        text: "GitHub",
-        link: "https://github.com/recoluan",
-        icon: "reco-github"
-      }
-    ]
+    items: [{
+      text: "GitHub",
+      link: "https://github.com/recoluan",
+      icon: "reco-github"
+    }]
   }
 ];
 module.exports = nav;
+
